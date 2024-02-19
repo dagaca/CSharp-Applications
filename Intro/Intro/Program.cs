4c4fcb438d23c8c438c27aa0222f675416e466aa
Damla Ağaça <dagacaa@gmail.com>
2024-02-19 21:42:22 +0300

    add intro and masktracking

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,71 @@
+﻿using Intro.Business;
+using Intro.Entitites;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+// variables -> camelCase
+bool isAuthenticated = true;
+
+Console.WriteLine(message1);
+
+
+// condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton-->Hoşgeldin Damla");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme Giriş Yap");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" }; // dbden gelecek
+// string[] loans2 = new string[];
+// loans2[0] = "Kredi 1";
+
+    //  start     condition     // increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C#";
+course1.Description = ".NET 8 vs...";
+course1.Price = 0;
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "JAVA";
+course2.Description = "Java 17 ....";
+course2.Price = 10;
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "Python";
+course3.Description = "Python 3.12...";
+course3.Price = 20;
+
+Course[] courses = { course1, course2, course3 };
+
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+Console.WriteLine("kod bitti");
---
+++ MaskTracking/Workaround/Program.cs
@@ -0,0 +1,103 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SelamVer(isim: "Engin");
+            SelamVer(isim: "Ahmet");
+            SelamVer(isim: "Ayşe");
+
+            int sonuc = Topla(6,58);
+
+            //Diziler / Arrays
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "İlker";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] {"Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] {"Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            Person person1 = new Person();
+            person1.FirstName = "DAMLA";
+            person1.LastName = "AĞAÇA";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            foreach (string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yeniSehirler1 = new List<string> { "Ankara", "İstanbul", "İzmir" };
+            yeniSehirler1.Add(item: "İzmir");
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim) 
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; // dbden gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            Console.WriteLine(tutar * 1.18);
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    // pascal casing
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
