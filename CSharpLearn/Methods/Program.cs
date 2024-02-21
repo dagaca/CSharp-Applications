9dfbc2eb31a5ba7c88019fee85b965e189391616
Damla Ağaça <dagacaa@gmail.com>
2024-02-21 23:10:03 +0300

    added csharplearn

---
+++ CSharpLearn/Methods/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+
+            //var result = Add2(20,25);
+            //Console.WriteLine(result);
+
+            //var result2 = Add3(20);
+            //Console.WriteLine(result2);
+
+            //int number1 = 20;
+            //Console.WriteLine("Number1 is : {0}", number1);
+            //int number2 = 100;
+            //var result3 = Add4(ref number1, number2);
+            //Console.WriteLine(result3);
+            //Console.WriteLine("ref Number1 is : {0}", number1);
+
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
+
+            Console.WriteLine(Add5(1,2,3,4,5));
+            Console.ReadLine();
+        }
+        
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+        static int Add3(int number1, int number2=30) // default parameter
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+        static int Add4(ref int number1, int number2) // same thing can be done using ref or out
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+
+        static int Add5(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
---
+++ CSharpLearn/TypesAndVariables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello World!");
+
+            double number5 = 10.4; // floating point
+            decimal number6 = 10.5m; // floating point
+            char character = 'A'; // a character UTF-16
+            bool condition = true; // true or false
+            byte number4 = 255; // 8 bit unsigned
+            short number3 = 32767; // 16 bit signed
+            int number1 = 2147483647; // 32 bit signed
+            long number2 = 9223372036854775807; // 64 bit signed
+            var number7 = 10;
+            number7 = 'A';
+            // number7 = "A"; // don't work
+
+            Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Number7 is {0}", number7);
+
+            Console.WriteLine("Condition is : {0}", condition);
+            Console.WriteLine("Character is : {0}", character);
+            Console.WriteLine("Character is : {0}", (int)character); // ASCII character
+
+            Console.WriteLine(Days.Friday);
+            Console.WriteLine((int)Days.Friday); // sequence number in the list
+
+            Console.ReadLine();
+        }
+    }
+
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+}
