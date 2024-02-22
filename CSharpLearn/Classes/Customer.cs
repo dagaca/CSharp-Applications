bdb25883c7f9f44670d8909d33ce328a927a59c8
Damla Ağaça <dagacaa@gmail.com>
2024-02-22 23:29:17 +0300

    added new csharplearn samples

---
+++ CSharpLearn/Classes/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+        //private string _firstName;
+        //public string FirstName 
+        //{
+        //    get { return "Mrs." + _firstName; }
+        //    set { _firstName = value; }
+        //}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
